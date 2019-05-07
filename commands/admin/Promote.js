4ac846e209052aab04f830bd41bfa1283d67c1f6
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-06 22:27:46 -0400

    Create Promote.js

---
+++ commands/admin/Promote.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js')
+
+module.exports = class addRoleCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name:"promote",
+            aliases: ["add-role", "arole"],
+            group: 'admin',
+            memberName: 'promote',
+            description: 'Adds a role to a user.',
+            args: [
+                {
+                    type:"user",
+                    prompt:"Which user would you like to add the role to?",
+                    key:"user",
+                },
+                {
+                    type:"role",
+                    prompt:"Which role would you like to add?",
+                    key:"role"
+                }
+            ]
+        })
+    }
+    run(msg, { user, role }) {
+
+        msg.guild.member(user).addRole(role)
+        msg.say('Successfully added ' + role + ' to ' + user)
+ 
+    
+    }
+}
