de9951346d86ff3e2b3c51e16994b7ed464af974
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-06 20:59:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const commando = require('discord.js-commando');
 const discord = require('discord.js');
 const client = new commando.CommandoClient({
   owner: '359835581456580618', // Your ID here.
-  commandPrefix: 'mb!', // The prefix of your bot.
+  commandPrefix: 'A!', // The prefix of your bot.
   unknownCommandResponse: true, // Set this to true if you want to send a message when a user uses the prefix not followed by a command
 });
 
