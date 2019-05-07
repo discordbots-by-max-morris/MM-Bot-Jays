54129876618357b2cebe4488d22913b5438e3dd9
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-06 22:43:11 -0400

    Create demote.js

---
+++ commands/admin/demote.js
@@ -0,0 +1,34 @@
+    
+const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js')
+
+module.exports = class removeRoleCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name:"demote",
+            aliases: ["remove-role", "rrole"],
+            group: 'moderation',
+            memberName: 'demote',
+            description: 'Removes a role from a user.',
+            args: [
+                {
+                    type:"user",
+                    prompt:"Which user would you like to remove the role from?",
+                    key:"user",
+                },
+                {
+                    type:"role",
+                    prompt:"Which role would you like to remove?",
+                    key:"role"
+                }
+            ]
+        })
+    }
+    run(msg, { user, role }) {
+
+        msg.guild.member(user).removeRole(role)
+        msg.say('Successfully took away the role ' + role + ' from ' + user)
+ 
+    
+    }
+}
