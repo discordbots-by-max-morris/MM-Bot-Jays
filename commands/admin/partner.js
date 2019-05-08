a8cac7c51adab2a498a498d2ebd2144923bb9a3b
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-08 00:31:41 -0400

    Update partner.js

--- commands/admin/partner.js
+++ commands/admin/partner.js
@@ -1,21 +1,20 @@
 const commando = require('discord.js-commando');
 const discord = require('discord.js')
-class StartAPatrolCommand extends commando.Command
-{
-    constructor(client)
-    {
-        super(client,{
-            name: 'partner',
-            group: 'admin',
-            memberName: 'partner',
-            description: '',
-               args: [
+module.exports = class WarnCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'warn',
+            group: 'moderation',
+            memberName: 'warn',
+            description: 'Warns a user and logs the warn to the mod logs.',
+            guildOnly: true,
+            args: [
                 {
                     key: 'member',
-                    prompt: 'What member do you want to partner?',
+                    prompt: 'What member do you want to warn?',
                     type: 'member'
                 }
-                   ]
+            ]
         });
     }
 
@@ -23,7 +22,7 @@ class StartAPatrolCommand extends commando.Command
     {
         var myInfo = new discord.RichEmbed()
         .setTitle("Partner!")
-        .setDescription(`Thank you ${member.user.tag} you ***are*** a partner with Ace's hangout server`)
+        .setDescription(`Thank you ${member} you ***are*** a partner with Ace's hangout server`)
         .setColor("RANDOM")
         .setThumbnail(message.author.avatarURL)
         .setAuthor("Ace's bot")
