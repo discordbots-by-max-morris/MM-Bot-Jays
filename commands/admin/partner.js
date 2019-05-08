f47a129dd92b01fc1d6e1bced8334def6ad526d5
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-08 00:21:11 -0400

    Update partner.js

--- commands/admin/partner.js
+++ commands/admin/partner.js
@@ -8,7 +8,14 @@ class StartAPatrolCommand extends commando.Command
             name: 'partner',
             group: 'admin',
             memberName: 'partner',
-            description: ''
+            description: '',
+               args: [
+                {
+                    key: 'member',
+                    prompt: 'What member do you want to warn?',
+                    type: 'member'
+                }
+                   ]
         });
     }
 
@@ -16,7 +23,7 @@ class StartAPatrolCommand extends commando.Command
     {
         var myInfo = new discord.RichEmbed()
         .setTitle("Partner!")
-        .setDescription(`Thank you ${message.author} you ***are*** a partner with Ace's hangout server`)
+        .setDescription(`Thank you ${args} you ***are*** a partner with Ace's hangout server`)
         .setColor("RANDOM")
         .setThumbnail(message.author.avatarURL)
         .setAuthor("Ace's bot")
