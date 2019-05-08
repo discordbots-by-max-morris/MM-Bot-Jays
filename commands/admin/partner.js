6e1da545a9f79520186fcc1cfa0a6d855b487a82
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-08 00:08:58 -0400

    Create partner.js

---
+++ commands/admin/partner.js
@@ -0,0 +1,27 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js')
+class StartAPatrolCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'partner',
+            group: 'admin',
+            memberName: 'partner',
+            description: ''
+        });
+    }
+
+    async run(message, args)
+    {
+        var myInfo = new discord.RichEmbed()
+        .setTitle("Partner!")
+        .setDescription(`Thank you ${User} you are not a partner with Ace's hangout server`)
+        .setColor("RANDOM")
+        .setThumbnail(message.author.avatarURL)
+        .setAuthor("Ace's bot")
+        message.channel.sendEmbed(myInfo);
+    }
+}
+
+module.exports = StartAPatrolCommand
