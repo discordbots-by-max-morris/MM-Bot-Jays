01a1e470455a7b99db7da6cb92601485e19d0fd7
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-09 18:33:24 -0400

    Update set-username.js

--- commands/util/set-username.js
+++ commands/util/set-username.js
@@ -20,7 +20,7 @@ module.exports = class SetUsernameCommand extends Command {
     }
     
     hasPermission(msg) {
-        return this.client.isGuildOwner(msg.author);
+        return this.client.owner(msg.author);
     }
 
     run(msg, args) {
