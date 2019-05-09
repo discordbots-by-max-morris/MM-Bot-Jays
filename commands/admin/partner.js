0f9ac25165232548e97162e0e5dd5e1da5a97aad
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-08 21:55:21 -0400

    Update partner.js

---
+++ commands/admin/partner.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+
+module.exports = class WarnCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'partner',
+            group: 'moderation',
+            memberName: 'partner',
+            description: 'A user want to partner.',
+            guildOnly: true,
+            args: [
+                {
+                    key: 'member',
+                    prompt: 'What member do you want to warn?',
+                    type: 'member'
+                }
+            ]
+        });
+    }
+    
+  async run(message, args) {
+    const annChan = this.client.channels.get('575762162555944961');
+    const embed = new RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setDescription(`The user: ${args.member} Wants to partner!`)
+      .setColor('RANDOM')
+      .setTimestamp();
+    annChan.sendEmbed(embed);
+    });
+  }
+};
