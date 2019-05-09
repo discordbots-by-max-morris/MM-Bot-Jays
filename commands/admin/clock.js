5fcd1c85325a578105904cf10ae23400b27f2878
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-09 19:06:30 -0400

    Update clock.js

--- commands/admin/clock.js
+++ commands/admin/clock.js
@@ -24,7 +24,7 @@ module.exports = class PartnerCommand extends Command {
     const annChan = this.client.channels.get('575762162555944961');
     const embed = new RichEmbed()
       .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(`The user: ${args.member} Just clocked in it wills show the time they clocked in at the bottom)
+      .setDescription(`The user: ${args.member} Just clocked in it wills show the time they clocked in at the bottom`)
       .setColor('RANDOM')
       .setTimestamp();
     annChan.sendEmbed(embed);
