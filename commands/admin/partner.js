dc9432b2f54309e76f201e90d06335b96b0086ba
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-17 00:21:06 -0400

    Update partner.js

--- commands/admin/partner.js
+++ commands/admin/partner.js
@@ -20,16 +20,34 @@ module.exports = class PartnerCommand extends Command {
                     key: 'string',
                     prompt: 'What is the reason?',
                     type: 'string'
+                },
+                {
+                    key: 'memberbot',
+                    prompt: 'What is the member count, bot count!',
+                    type: 'string'
+                },
+                {
+                    key: 'server',
+                    prompt: 'What is the the server?',
+                    type: 'string'
+                },
+                {
+                    key: 'added',
+                    prompt: 'What is the anything else you want to add?',
+                    type: 'string'
                 }
             ]
         });
     }
     
   async run(message, args) {
-    const annChan = this.client.channels.get('553482011218804736');
+    const annChan = this.client.channels.get('575762162555944961');
     const embed = new RichEmbed()
       .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(`The user: ${args.member} Wants to partner!\n With the reason ${args.string}`)
+      .setDescription(`The user: ${args.member} Wants to partner!`)
+      .addField('The total members and bots are:', `${args.memberbot} that is the total member and bot count!`)
+      .addField('The server link:', `${args.server} that is the server!`)
+      .addField('Any other added info:',`${args.added}\n That is all the add info!`)
       .setColor('RANDOM')
       .setTimestamp();
     annChan.sendEmbed(embed);
