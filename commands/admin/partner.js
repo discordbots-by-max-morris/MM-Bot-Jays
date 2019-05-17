1055690fb9dcd12c8304e6d7fdb67e0aca1f451c
MaxMorris121 <46467057+MaxMorris121@users.noreply.github.com>
2019-05-17 00:09:07 -0400

    Update partner.js

--- commands/admin/partner.js
+++ commands/admin/partner.js
@@ -13,18 +13,23 @@ module.exports = class PartnerCommand extends Command {
             args: [
                 {
                     key: 'member',
-                    prompt: 'What member do you want to warn?',
+                    prompt: 'What is the user who wants to partner?',
                     type: 'member'
+                },
+                {
+                    key: 'string',
+                    prompt: 'What is the reason?',
+                    type: 'string'
                 }
             ]
         });
     }
     
   async run(message, args) {
-    const annChan = this.client.channels.get('575762162555944961');
+    const annChan = this.client.channels.get('553482011218804736');
     const embed = new RichEmbed()
       .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(`The user: ${args.member} Wants to partner!`)
+      .setDescription(`The user: ${args.member} Wants to partner!\n With the reason ${args.string}`)
       .setColor('RANDOM')
       .setTimestamp();
     annChan.sendEmbed(embed);
